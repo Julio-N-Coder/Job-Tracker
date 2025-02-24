9deff3627a69bf6ca76cc5f15a2cc7f7f008ffc3
Julio <Julio.Navarro.Coder@gmail.com>
2025-02-23 16:52:25 -0800

    frontpage and about

--- FrontEnd/src/about/about.tsx
+++ FrontEnd/src/about/about.tsx
@@ -1,7 +1,14 @@
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="container mx-auto rounded-b-xl bg-base-200 p-2 space-y-4">
+      <h1 className="text-center text-6xl font-bold">
+        Welcome To Job Tracker!
+      </h1>
+      <p className="text-xl">
+        This web application helps job seekers keep track of the jobs they've
+        applied to, making it easier to keep track of the jobs you applied to,
+        and stay on top of their job search.
+      </p>
     </div>
   );
 }
--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -1,7 +1,17 @@
+import picture from "./assets/job-search-picture.svg";
+
 export default function Index() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex justify-around pr-8">
+      <img
+        className="w-170 h-170"
+        src={picture}
+        alt="Image of person looking for a job"
+      />
+      <h1 className="text-9xl font-bold flex flex-col items-center justify-center">
+        <span>Job</span>
+        <span>Tracker</span>
+      </h1>
     </div>
   );
 }
