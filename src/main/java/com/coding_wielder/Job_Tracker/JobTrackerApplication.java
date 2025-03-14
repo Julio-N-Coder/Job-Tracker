720202856f5dde8b246d8b6a01698dbe010826f4
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-13 18:15:44 -0700

    added command line runner for pgadmin4 web URL in development profile

--- src/main/java/com/coding_wielder/Job_Tracker/JobTrackerApplication.java
+++ src/main/java/com/coding_wielder/Job_Tracker/JobTrackerApplication.java
@@ -1,13 +1,28 @@
 package com.coding_wielder.Job_Tracker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class JobTrackerApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(JobTrackerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(JobTrackerApplication.class, args);
+	}
+
+	@Bean
+	@Profile("dev")
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			logger.info("pgadmin4 web URL: http://localhost:5050/");
+		};
 	}
 
 }
