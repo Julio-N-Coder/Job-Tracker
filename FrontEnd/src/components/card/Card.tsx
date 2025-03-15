dddcb81ef9970d0d2bc836dd6fb3d37e897db53e
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-14 23:09:09 -0700

    Added basic Job card layout and job submission modal

--- FrontEnd/src/components/card/Card.tsx
+++ FrontEnd/src/components/card/Card.tsx
@@ -1,15 +1,26 @@
 import { Job } from "../../types";
 
-export default function Card({jobId, jobTitle, company, status, appliedDate, userId}: Job) {
+export default function Card({
+  jobId,
+  jobTitle,
+  company,
+  status,
+  appliedDate,
+  userId,
+}: Job) {
   return (
-    <div>
-      <h3>Card</h3>
-      <h4>{jobId}</h4>
-      <h4>{jobTitle}</h4>
-      <h4>{company}</h4>
-      <h4>{status}</h4>
-      <h4>{appliedDate.toISOString()}</h4>
-      <h4>{userId}</h4>
+    <div
+      className="bg-base-300 p-2 rounded text-lg space-y-2"
+      id={jobId}
+      data-userid={userId}
+    >
+      <h3 className="text-3xl text-center font-bold">{company}</h3>
+      <p>Job Title: {jobTitle}</p>
+      <p>Status: {status}</p>
+      <div className="flex flex-col items-center">
+        <p className="underline">Applied Date</p>
+        <p>{appliedDate.toISOString().split("T")[0]}</p>
+      </div>
     </div>
   );
 }
--- FrontEnd/src/jobs-page/JobsPage.tsx
+++ FrontEnd/src/jobs-page/JobsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Card from "../components/card/Card";
 import { useNavigate } from "react-router";
 import { Job } from "../types";
@@ -36,18 +36,64 @@ export default function JobsPage() {
   }, []);
 
   return (
-    <div className="container mx-auto bg-base-200">
-      {/* display jobs card data */}
-      {jobs.map(() => (
-        <Card
-          jobId="id"
-          jobTitle="jobTitle"
-          company="company"
-          status="status"
-          appliedDate={new Date()}
-          userId="userId"
-        />
-      ))}
+    <div className="container mx-auto bg-base-200 rounded-b-lg">
+      <div className="bg-base-300 flex justify-between p-2">
+        <h2 className="font-bold text-2xl">Jobs</h2>
+        <button
+          className="btn btn-accent text-xl"
+          onClick={() =>
+            (
+              document.getElementById("add-job-id") as HTMLDialogElement
+            ).showModal()
+          }
+        >
+          Add Job
+        </button>
+      </div>
+      <div className="p-2 flex justify-around flex-wrap gap-2">
+        {/* display jobs card data */}
+        {jobs.map((job) => (
+          <Card
+            jobId={job.jobId}
+            jobTitle={job.jobTitle}
+            company={job.company}
+            status={job.status}
+            appliedDate={job.appliedDate}
+            userId={job.userId}
+          />
+        ))}
+      </div>
+      <AddJobModel />
     </div>
+  );
+}
+
+function AddJobModel() {
+  function handleJobSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("send post request here");
+  }
+
+  return (
+    <dialog id="add-job-id" className="modal modal-bottom sm:modal-middle">
+      <div className="modal-box">
+        <form method="dialog">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="">Press ESC key or click the button below to close</p>
+        <p className="">Model not setup. add form to add a job</p>
+        <form
+          id="add-job-form"
+          onSubmit={(event) => {
+            handleJobSubmit(event);
+          }}
+        >
+          <button className="btn btn-accent">Submit</button>
+        </form>
+      </div>
+    </dialog>
   );
 }
