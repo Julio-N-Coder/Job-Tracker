496befca8df51e03737124d8ab1a0d6540755912
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-23 12:02:34 -0700

    made svg file for small svg's

--- FrontEnd/src/pages/auth-pages/AuthLayout.tsx
+++ FrontEnd/src/pages/auth-pages/AuthLayout.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import { FormEvent, ChangeEvent, useState } from "react";
 import ErrorPopup from "../../components/ErrorPopup";
 import SubmitButton from "../../components/SubmitButton";
+import { UserSvg, KeySvg } from "../../components/svg/Svg";
 
 interface TokenResponse {
   token: string;
@@ -173,47 +174,5 @@ export default function AuthLayout({ type }: { type: string }) {
         </div>
       </div>
     </div>
-  );
-}
-
-function KeySvg() {
-  return (
-    <svg
-      className="h-[1em] opacity-50"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <g
-        strokeLinejoin="round"
-        strokeLinecap="round"
-        strokeWidth="2.5"
-        fill="none"
-        stroke="currentColor"
-      >
-        <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-        <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
-      </g>
-    </svg>
-  );
-}
-
-function UserSvg() {
-  return (
-    <svg
-      className="h-[1em] opacity-50"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <g
-        strokeLinejoin="round"
-        strokeLinecap="round"
-        strokeWidth="2.5"
-        fill="none"
-        stroke="currentColor"
-      >
-        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-        <circle cx="12" cy="7" r="4"></circle>
-      </g>
-    </svg>
   );
 }
