7a30de6cbf6b403e8a5dcfeace76ba12a18d1029
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-23 14:43:09 -0700

    delete account model

--- FrontEnd/src/pages/profilePage/ProfilePage.tsx
+++ FrontEnd/src/pages/profilePage/ProfilePage.tsx
@@ -2,13 +2,21 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import areTokensValid from "../../lib/tokens";
 import { User } from "../../types";
+import FormModel from "../../components/baseModels/FormModel";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export default function ProfilePage() {
   const navigate = useNavigate();
-  //   let [userId, setUserId] = useState("");
   let [userName, setUserName] = useState("");
+  //   let [userId, setUserId] = useState("");
+  let [hidePopUp, setHidePopUp] = useState(true);
+  let [popUpMessage, setPopUpMessage] = useState("");
+  const modelId = "delete-acccount-model-id";
+
+  function deleteAccount() {
+    console.log("Delete account here");
+  }
 
   // fetch user info and dislay it
   useEffect(() => {
@@ -43,7 +51,27 @@ export default function ProfilePage() {
     <div className="container mx-auto bg-base-200 rounded-b-lg p-2 flex flex-col items-center gap-6">
       <h2 className="text-5xl font-bold">Profile Page</h2>
       <h3 className="text-4xl font-bold">Username: {userName}</h3>
-      <button className="btn btn-error text-2xl">Delete Account</button>
+      <button
+        className="btn btn-error text-2xl"
+        onClick={() =>
+          (document.getElementById(modelId) as HTMLDialogElement).showModal()
+        }
+      >
+        Delete Account
+      </button>
+      <FormModel id={modelId} hidePopUp={hidePopUp} popUpMessage={popUpMessage}>
+        <div className="flex flex-col py-2 items-center gap-2">
+          <h4 className="text-xl font-bold">
+            Are you sure you want to delete your account?
+          </h4>
+          <button
+            className="btn btn-error text-lg w-42"
+            onClick={deleteAccount}
+          >
+            Delete Account
+          </button>
+        </div>
+      </FormModel>
     </div>
   );
 }
