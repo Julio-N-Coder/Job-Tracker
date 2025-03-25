e31af6b5f5f149fa517a58843b54de718546ed07
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-25 11:58:26 -0700

    fixed UserControllerTest

--- src/test/java/com/coding_wielder/Job_Tracker/users/UserControllerTest.java
+++ src/test/java/com/coding_wielder/Job_Tracker/users/UserControllerTest.java
@@ -1,52 +1,57 @@
 package com.coding_wielder.Job_Tracker.users;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Optional;
-import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.coding_wielder.Job_Tracker.configuration.BaseControllerTestUnit;
 
-@WebMvcTest(UserController.class)
-public class UserControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class UserControllerTest extends BaseControllerTestUnit {
 
-  @Autowired
-  private MockMvc mockMvc;
+  private User oneUser = new User(userId, "fakeUserName", "Fake Hashed Password");
 
-  @Autowired
-  private ObjectMapper objectMapper;
-
-  @MockitoBean
-  private UserRepository userRepository;
-
-  private final UUID id = UUID.fromString("11111111-1111-1111-1111-111111111111");
-  private User oneUser = new User(id, "fakeUserName", "Fake Hashed Password");
+  @BeforeEach
+  void temp() {
+    token = jwtUtil.generateToken(userId);
+  }
 
   @Test
   void getUserTest() throws Exception {
     String oneUserJson = objectMapper.writeValueAsString(oneUser);
-    when(userRepository.findById(id)).thenReturn(Optional.of(oneUser));
+    when(userRepository.findById(userId)).thenReturn(Optional.of(oneUser));
 
-    mockMvc.perform(get("/user/{id}", id)
+    mvc.perform(get("/user")
+        .header("Authorization", "Bearer " + token)
         .contentType(MediaType.APPLICATION_JSON)
         .content(oneUserJson)).andExpect(status().isOk())
-        .andExpect(jsonPath("$.id").value(id.toString()));
+        .andExpect(jsonPath("$.id").value(userId.toString()));
 
-    when(userRepository.findById(id)).thenReturn(Optional.empty());
+    when(userRepository.findById(userId)).thenReturn(Optional.empty());
 
-    mockMvc.perform(get("/user/{id}", id)
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(oneUserJson)).andExpect(status().isNotFound());
+    assertThrows(UsernameNotFoundException.class, () -> {
+      mvc.perform(get("/user")
+          .header("Authorization", "Bearer " + token)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(oneUserJson))
+          .andExpect(status().isNotFound());
+    });
   }
 
 }
