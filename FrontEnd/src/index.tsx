e2a55ae38f89901feadcc2c63336e982974fcfb4
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-26 13:26:13 -0700

    Made Front Page responsive

--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -2,13 +2,13 @@ import picture from "./assets/job-search-picture.svg";
 
 export default function Index() {
   return (
-    <div className="flex justify-around pr-8">
+    <div className="flex flex-col-reverse items-center lg:flex-row justify-around lg:pr-8">
       <img
-        className="w-170 h-170"
+        className="w-100 h-100 sm:w-130 sm:h-130 xl:w-170 xl:h-170"
         src={picture}
         alt="Image of person looking for a job"
       />
-      <h1 className="text-9xl font-bold flex flex-col items-center justify-center">
+      <h1 className="text-7xl xsm:text-8xl sm:text-9xl font-bold flex flex-col items-center justify-center">
         <span>Job</span>
         <span>Tracker</span>
       </h1>
