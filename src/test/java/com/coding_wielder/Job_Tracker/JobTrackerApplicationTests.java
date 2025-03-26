3a0eac5ec572f78e4968df064662146890213c9e
Julio-N-Coder <julio.navarro.coder@gmail.com>
2025-03-25 19:23:25 -0700

    added JobController Test for main test

--- src/test/java/com/coding_wielder/Job_Tracker/JobTrackerApplicationTests.java
+++ src/test/java/com/coding_wielder/Job_Tracker/JobTrackerApplicationTests.java
@@ -2,10 +2,13 @@ package com.coding_wielder.Job_Tracker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -23,9 +26,11 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.coding_wielder.Job_Tracker.jobs.Job;
 import com.coding_wielder.Job_Tracker.jobs.JobRepository;
 import com.coding_wielder.Job_Tracker.lib.TestTokenLib;
 import com.coding_wielder.Job_Tracker.security.JwtUtil;
+import com.coding_wielder.Job_Tracker.users.User;
 import com.coding_wielder.Job_Tracker.users.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -56,7 +61,7 @@ class JobTrackerApplicationTests {
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
-	JdbcClient jdbcClient;
+	private JdbcClient jdbcClient;
 	@Autowired
 	private JwtUtil jwtUtil;
 	@Autowired
@@ -72,6 +77,9 @@ class JobTrackerApplicationTests {
 	private static final UUID userId = UUID.fromString("11111111-1111-1111-1111-111111111112");
 	private static final String userName = "TestUserName";
 	private static final String password = "TestPassWord";
+	private static final UUID jobId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+	private static final Job testJob = new Job(jobId, "TestJobTitle", "TestCompany", "TestStatus", LocalDateTime.now(),
+			userId);
 
 	@BeforeAll
 	static void setUp(@Autowired JwtUtil jwtUtil, @Autowired JdbcClient jdbcClient,
@@ -85,6 +93,18 @@ class JobTrackerApplicationTests {
 				.param("id", userId)
 				.param("username", userName)
 				.param("password", passwordEncoder.encode(password))
+				.update();
+
+		// Store test Job
+		jdbcClient
+				.sql("INSERT INTO jobs (id, job_title, company, status, applied_date, user_id) " +
+						"VALUES (:id, :jobTitle, :company, :status, :appliedDate, :userId)")
+				.param("id", jobId)
+				.param("jobTitle", testJob.jobTitle())
+				.param("company", testJob.company())
+				.param("status", testJob.status())
+				.param("appliedDate", testJob.appliedDate())
+				.param("userId", testJob.userId())
 				.update();
 	}
 
@@ -144,7 +164,125 @@ class JobTrackerApplicationTests {
 		assertEquals("refresh_token", refreshTokenClaims.get("token_use"),
 				"Refresh token use should be 'refresh_token'");
 	}
+
+	/* Job Controller Test */
+	@Test
+	void addNewJobTest() throws Exception {
+		RequestJob requestJob = new RequestJob("Software Engineer", "TechCorp", "Applied");
+
+		mvc.perform(post("/job")
+				.header("Authorization", "Bearer " + token)
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(requestJob)))
+				.andExpect(status().isOk());
+
+		assertTrue(jobRepository.findAll().stream()
+				.anyMatch(job -> job.jobTitle().equals("Software Engineer")
+						&& job.company().equals("TechCorp")
+						&& job.status().equals("Applied")),
+				"Job was not Saved to Database");
+	}
+
+	@Test
+	void getJobsTest() throws Exception {
+		mvc.perform(get("/jobs")
+				.header("Authorization", "Bearer " + token))
+				.andExpect(status().isOk());
+
+		assertTrue(jobRepository.findAll().size() > 0);
+	}
+
+	@Test
+	void getJobByIdTest() throws Exception {
+		String responseJob = mvc.perform(get("/job/{jobId}", jobId)
+				.header("Authorization", "Bearer " + token))
+				.andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
+
+		Job job = objectMapper.readValue(responseJob, Job.class);
+
+		assertEquals(jobId, job.id(), "Job ID does not match");
+		assertEquals(testJob.jobTitle(), job.jobTitle(), "Job title does not match");
+		assertEquals(testJob.company(), job.company(), "Job company does not match");
+		assertEquals(testJob.status(), job.status(), "Job status does not match");
+		assertEquals(testJob.userId(), job.userId(), "Job user ID does not match");
+	}
+
+	@Test
+	void updateJobTest() throws Exception {
+		UUID oldJobId = UUID.fromString("21111111-1111-1111-1111-111111111111");
+		UUID oldUserId = UUID.fromString("22111111-1111-1111-1111-111111111111");
+
+		User oldUser = new User(oldUserId, "OldUserName", "password");
+		saveUserWithClient(oldUser);
+
+		Job oldJob = new Job(oldJobId, "OldJobTitle", "OldCompany", "OldStatus", LocalDateTime.now(), oldUserId);
+		RequestJob updatedRequestJob = new RequestJob("UpdatedJobTitle", "UpdatedCompany", "Updatedstatus");
+
+		saveJobWithClient(oldJob);
+
+		mvc.perform(put("/job/{jobId}", oldJobId)
+				.header("Authorization", "Bearer " + jwtUtil.generateToken(oldUserId))
+				.contentType("application/json")
+				.content(objectMapper.writeValueAsString(updatedRequestJob)))
+				.andExpect(status().isOk());
+
+		Job updatedJob = jobRepository.findById(oldJobId).orElseThrow(() -> new RuntimeException("Job not found"));
+
+		assertEquals(updatedRequestJob.jobTitle(), updatedJob.jobTitle(), "Job title was not updated");
+		assertEquals(updatedRequestJob.company(), updatedJob.company(), "Job company was not updated");
+		assertEquals(updatedRequestJob.status(), updatedJob.status(), "Job status was not updated");
+
+		userRepository.delete(oldUser);
+	}
+
+	@Test
+	void deleteJobTest() throws Exception {
+		UUID oldJobId = UUID.fromString("21111111-1111-1111-1111-111111111111");
+		UUID oldUserId = UUID.fromString("22111111-1111-1111-1111-111111111111");
+
+		User oldUser = new User(oldUserId, "OldUserName", "password");
+		saveUserWithClient(oldUser);
+
+		Job oldJob = new Job(oldJobId, "OldJobTitle", "OldCompany", "OldStatus", LocalDateTime.now(), oldUserId);
+
+		saveJobWithClient(oldJob);
+
+		mvc.perform(delete("/job/{jobId}", oldJobId)
+				.header("Authorization", "Bearer " + jwtUtil.generateToken(oldUserId)))
+				.andExpect(status().isOk());
+
+		assertTrue(jobRepository.findById(oldJobId).isEmpty(), "Job Was not deleted");
+	}
+
+	private void saveJobWithClient(Job job) {
+		jdbcClient
+				.sql("INSERT INTO jobs (id, job_title, company, status, applied_date, user_id) " +
+						"VALUES (:id, :jobTitle, :company, :status, :appliedDate, :userId)")
+				.param("id", job.id())
+				.param("jobTitle", job.jobTitle())
+				.param("company", job.company())
+				.param("status", job.status())
+				.param("appliedDate", job.appliedDate())
+				.param("userId", job.userId())
+				.update();
+	}
+
+	private void saveUserWithClient(User user) {
+		jdbcClient
+				.sql("INSERT INTO users (id, username, hashed_password) VALUES (:id, :username, :password)")
+				.param("id", user.id())
+				.param("username", user.username())
+				.param("password", user.hashedPassword())
+				.update();
+	}
 }
 
 record TokenResponse(String token, String refresh_token) {
+}
+
+record RequestJob(
+		String jobTitle,
+		String company,
+		String status) {
 }
